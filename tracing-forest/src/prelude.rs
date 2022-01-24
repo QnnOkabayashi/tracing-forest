bce24a26b7962e5761535d0867e1ff4a4b0f87cd
William Brown <wbrown@suse.de>
2022-01-24 10:14:59 +1000

    Temp update

--- tracing-forest/src/prelude.rs
+++ tracing-forest/src/prelude.rs
@@ -1,6 +1,8 @@
 //! The module re-exports a number of useful components that you may wish
 //! to consume.
 
+pub use tracing::Instrument;
+
 pub use tracing::{
     debug,
     debug_span,
@@ -15,6 +17,28 @@ pub use tracing::{
     warn,
     warn_span,
 };
-pub use tracing::Instrument;
 
-pub use tracing_subscriber::filter::LevelFilter;
+// As these macros are macro_export, they are published at the crate root.
+/*
+#[cfg(feature = "uuid")]
+pub use crate::{
+    uuid_debug_span,
+    uuid_error_span,
+    uuid_info_span,
+    uuid_span,
+    uuid_trace_span,
+    uuid_warn_span,
+};
+*/
+
+#[cfg(feature = "attributes")]
+pub use tracing;
+#[cfg(feature = "attributes")]
+pub use tracing::instrument;
+
+pub mod filter {
+    pub use tracing_subscriber::filter::LevelFilter;
+    #[cfg(feature = "env-filter")]
+    pub use tracing_subscriber::filter::EnvFilter;
+}
+
