3b27d7bd43314b7810371fbda9f46fe86b132201
Quinn Okabayashi <qokabay1@swarthmore.edu>
2022-03-03 11:48:06 -0500

    address #3

---
+++ tracing-forest/tests/spawned_tasks.rs
@@ -0,0 +1,31 @@
+use tokio::time::{sleep, Duration};
+
+#[tokio::test(flavor = "multi_thread")]
+async fn spawned_tasks() -> Result<(), Box<dyn std::error::Error>> {
+    let logs = tracing_forest::capture()
+        .set_global(true)
+        .on_registry()
+        .on(async {
+            tracing::error!("Waiting on signal");
+            let handle = tokio::spawn(async {
+                tracing::error!("Test message");
+            });
+            sleep(Duration::from_millis(100)).await;
+            handle.await.unwrap();
+            tracing::error!("Stopping");
+        })
+        .await;
+
+    assert!(logs.len() == 3);
+
+    let waiting = logs[0].event()?;
+    assert!(waiting.message() == "Waiting on signal");
+
+    let test = logs[1].event()?;
+    assert!(test.message() == "Test message");
+
+    let stopping = logs[2].event()?;
+    assert!(stopping.message() == "Stopping");
+
+    Ok(())
+}
