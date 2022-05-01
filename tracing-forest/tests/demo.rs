5df674c5b21490549704b39d6b37c73110b9560f
Quinn Okabayashi <qokabay1@swarthmore.edu>
2022-05-01 13:23:08 -0400

    cleaned up `demo` tests

--- tracing-forest/tests/demo.rs
+++ tracing-forest/tests/demo.rs
@@ -15,60 +15,57 @@ fn test_manual_with_json() {
     });
 }
 
+fn pretty_tag(event: &Event) -> Option<Tag> {
+    let level = *event.metadata().level();
+    let target = event.metadata().target();
+
+    Some(match target {
+        "security" if level == Level::ERROR => {
+            Tag::build(|builder| builder.icon('🔐').prefix(target).suffix("critical"))
+        }
+        "security" if level == Level::INFO => {
+            Tag::build(|builder| builder.icon('🔓').prefix(target).suffix("access"))
+        }
+        "admin" | "request" | "filter" => Tag::build(|builder| builder.prefix(target).level(level)),
+        _ => return None,
+    })
+}
+
 #[test]
 #[ignore]
 fn pretty_example() {
-    let _guard = {
-        let layer = ForestLayer::new(Printer::default(), |event: &Event| {
-            let level = *event.metadata().level();
-            let target = event.metadata().target();
+    let _guard = tracing::subscriber::set_default({
+        let layer = ForestLayer::new(Printer::new(), pretty_tag);
 
-            let tag = match target {
-                "security" if level == Level::ERROR => {
-                    Tag::build(|builder| builder.icon('🔐').prefix(target).suffix("critical"))
-                }
-                "security" if level == Level::INFO => {
-                    Tag::build(|builder| builder.icon('🔓').prefix(target).suffix("access"))
-                }
-                "admin" | "request" | "filter" => {
-                    Tag::build(|builder| builder.prefix(target).level(level))
-                }
-                _ => return None,
-            };
-
-            Some(tag)
-        });
-
-        let subscriber = Registry::default().with(layer);
-        tracing::subscriber::set_default(subscriber)
-    };
+        Registry::default().with(layer)
+    });
 
     info_span!("try_from_entry_ro").in_scope(|| {
-            info_span!("server::internal_search").in_scope(|| {
-                info!(target: "filter", "Some filter info...");
-                info_span!("server::search").in_scope(|| {
-                    info_span!("be::search").in_scope(|| {
-                        info_span!("be::search -> filter2idl").in_scope(|| {
-                            info_span!("be::idl_arc_sqlite::get_idl").in_scope(|| {
-                                info!(target: "filter", "Some filter info...");
-                            });
-                            info_span!("be::idl_arc_sqlite::get_idl").in_scope(|| {
-                                error!(target: "admin", "On no, an admin error occurred :(");
-                                debug!("An untagged debug log");
-                                info!(target: "admin", alive = false, status = "sad", "there's been a big mistake")
-                            });
+        info_span!("server::internal_search").in_scope(|| {
+            info!(target: "filter", "Some filter info...");
+            info_span!("server::search").in_scope(|| {
+                info_span!("be::search").in_scope(|| {
+                    info_span!("be::search -> filter2idl").in_scope(|| {
+                        info_span!("be::idl_arc_sqlite::get_idl").in_scope(|| {
+                            info!(target: "filter", "Some filter info...");
+                        });
+                        info_span!("be::idl_arc_sqlite::get_idl").in_scope(|| {
+                            error!(target: "admin", "On no, an admin error occurred :(");
+                            debug!("An untagged debug log");
+                            info!(target: "admin", alive = false, status = "sad", "there's been a big mistake")
                         });
                     });
-                    info_span!("be::idl_arc_sqlite::get_identry").in_scope(|| {
-                        error!(target: "security", "A security critical log");
-                        info!(target: "security", "A security access log");
-                    });
                 });
-                info_span!("server::search<filter_resolve>").in_scope(|| {
-                    warn!(target: "filter", "Some filter warning");
+                info_span!("be::idl_arc_sqlite::get_identry").in_scope(|| {
+                    error!(target: "security", "A security critical log");
+                    info!(target: "security", "A security access log");
                 });
-
             });
-            trace!("Finished!");
+            info_span!("server::search<filter_resolve>").in_scope(|| {
+                warn!(target: "filter", "Some filter warning");
+            });
+
         });
+        trace!("Finished!");
+    });
 }
